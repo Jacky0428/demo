140b186d7136c2928edd5772f859fed81191b41e
Jacky0428 <jt26419701@yahoo.com.tw>
2022-06-08 23:16:48 +0800

    commit test - 建立一個 Controller

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 建立一個 Controller (API Service)
+ * test url: http://localhost:8080/hello
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String handle01(){
+        return "Hello, Spring Boot 2!";
+    }
+}
