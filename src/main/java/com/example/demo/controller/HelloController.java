fefba7416336600f06bf00a47bc7922663c41eaf
Seth <moneycat2128@hotmail.com>
2022-06-09 11:29:54 +0800

    Seth commit test!!!!!

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String handle01(){
-        return "Hello, Spring Boot 2!";
+        return "Hello, Spring Boot 2! Seth commit Test!";
     }
 }
